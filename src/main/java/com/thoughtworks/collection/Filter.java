0423f01a0d0284cf30c1b8674b81e8e21fcc41c3
TREVOR JOVE CRUZ (SHP-IRIS-ISD-OOCLL/MNL) <CRUZTR@OOCL.COM>
2019-10-10 20:29:25 +0800

    implement methods in Filter

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,30 +2,49 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
-    List<Integer>  array;
+    List<Integer> integerList;
 
-    public Filter(List<Integer> array) {
-     this.array = array;
+    public Filter(List<Integer> integerList) {
+     this.integerList = integerList;
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return integerList.stream()
+                .filter(num -> num%2==0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return integerList.stream()
+                .filter(num -> num%3==0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        List<Integer> commonList = new ArrayList<>();
+
+        firstList.stream()
+                .filter(num -> ifContains(secondList, num))
+                .forEach(commonList::add);
+
+        return commonList;
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        return integerList.stream()
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private boolean ifContains(List<Integer> intList, int num) {
+        return intList.stream()
+                .anyMatch(integer -> integer == num);
     }
 }
